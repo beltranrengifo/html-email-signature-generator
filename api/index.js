be31c80cd363e4eb6891b63de1c9b680644a14ef
Beltran Rengifo <beltran.rengifo@liferay.com>
2022-11-21 13:35:09 +0100

    feat: testing how to wok with html2canvas proxy

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const url = require('url')
-const cors = require('cors')
-const request = require('request')
+/* import express from 'express'
+import url from 'url'
+import cors from 'cors'
+import request from 'request'
 
-function validUrl(req, res, next) {
+const validUrl = (req, res, next) => {
   if (!req.query.url) {
     next(new Error('No url specified'))
   } else if (
@@ -17,7 +17,7 @@ function validUrl(req, res, next) {
   }
 }
 
-module.exports = () => {
+const getImageAsBase64 = () => {
   const app = express.Router()
   app.get('/', cors(), validUrl, (req, res, next) => {
     switch (req.query.responseType) {
@@ -49,3 +49,29 @@ module.exports = () => {
 
   return app
 }
+
+export default getImageAsBase64 */
+
+const app = require('express')()
+const cors = require('cors')
+const request = require('request')
+
+app.get('/api', cors(), (req, res, next) => {
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+  request(
+    { url: req.query.url, encoding: 'binary' },
+    (error, response, body) => {
+      if (error) {
+        return next(error)
+      }
+      res.send(
+        `data:${response.headers['content-type']};base64,${Buffer.from(
+          body,
+          'binary'
+        ).toString('base64')}`
+      )
+    }
+  )
+})
+
+module.exports = app
