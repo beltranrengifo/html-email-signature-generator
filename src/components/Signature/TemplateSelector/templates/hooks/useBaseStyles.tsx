6af3c36b60f6dd35b0aeff4a68370fe38f3b67fa
Beltran Rengifo <beltran.rengifo@liferay.com>
2022-11-23 19:53:16 +0100

    wip: color management feature

--- src/components/Signature/TemplateSelector/templates/hooks/useBaseStyles.tsx
+++ src/components/Signature/TemplateSelector/templates/hooks/useBaseStyles.tsx
@@ -1,14 +1,17 @@
 import { useColorModeValue } from '@chakra-ui/react'
+import { useState } from 'react'
 
 const useBaseStyles = (colorLight = '#473741', colorDark = '#d2d2d2') => {
   const color = useColorModeValue(colorLight, colorDark)
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+  const [currentColor, setCurrentColor] = useState(color)
 
   return {
     WebkitMarginBefore: 0,
     WebkitMarginAfter: 0,
     fontFamily: 'Trubuchet, Arial, sans-serif',
     fontSize: '14px',
-    color,
+    color: currentColor,
     letterSpacing: '1px',
     margin: '12px 0 0 0',
   }
